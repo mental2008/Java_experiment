be03c46d7f5bcf16417e3d1c84d78ea6978f9e40
mental2008 <mental2008@163.com>
2017-04-13 18:20:13 +0800

    Experiment 1 二分法求函数零点

---
+++ Experiment1/src/Main.java
@@ -0,0 +1,28 @@
+
+public class Main {
+
+	public static double f(double x) {
+		return x * x * x - 10 * x + 23;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double l = -10.0, r = 5.0;
+		double exps = 0.001;
+		while(Math.abs(l - r) > exps) {
+			double mid = (l + r) / 2;
+			if(f(mid) == 0) {
+				l = mid;
+				break;
+			}
+			else if(f(mid) * f(l) <= 0) {
+				r = mid;
+			}
+			else {
+				l = mid;
+			}
+		}
+		System.out.println("The answer is " + l);
+	}
+
+}
