bfdfe24a41118e4a27877dd2530570a14a07378e
mental <mental2008@163.com>
2017-06-16 01:19:10 +0800

    利用Jsoup爬虫框架 爬取网易首页的新闻

--- Experiment8/src/Reptile.java
+++ Experiment8/src/Reptile.java
@@ -33,13 +33,224 @@ public class Reptile {
 
         PrintWriter printWriter = new PrintWriter(new FileWriter(file), true);
 
-        Elements links = doc.select("a");
+//        Elements links = doc.select("a");
 
-        for(org.jsoup.nodes.Element link : links) {
-            if(!link.hasText()) continue;
-            printWriter.println(link.attr("abs:href") + " " + link.text());
-//            printWriter.println(link.text());
+        Elements ListDiv = doc.getElementsByAttributeValue("class","scroll_column_bd");
+        printWriter.println("新闻各有态度");
+        for(org.jsoup.nodes.Element div : ListDiv) {
+            Elements links = div.getElementsByTag("a");
+            for(org.jsoup.nodes.Element link : links) {
+                printWriter.println(link.text() + "\t" + link.attr("href"));
+            }
         }
+        printWriter.println();
+
+        ListDiv = doc.getElementsByAttributeValue("class", "mod_top_news2");
+        printWriter.println("头条新闻");
+        for(org.jsoup.nodes.Element div : ListDiv) {
+            Elements links = div.getElementsByTag("a");
+            for(org.jsoup.nodes.Element link : links) {
+                printWriter.println(link.text() + "\t" + link.attr("href"));
+            }
+        }
+        printWriter.println();
+
+        ListDiv = doc.getElementsByAttributeValue("class", "mod_important_news none");
+        printWriter.println("特别报道");
+        for(org.jsoup.nodes.Element div : ListDiv) {
+            Elements links = div.getElementsByTag("a");
+            for(org.jsoup.nodes.Element link : links) {
+                printWriter.println(link.text() + "\t" + link.attr("href"));
+            }
+        }
+        printWriter.println();
+
+        ListDiv = doc.getElementsByAttributeValue("class", "mod_netes_origina");
+//        printWriter.println("网易独家:");
+        for(org.jsoup.nodes.Element div : ListDiv) {
+            Elements links = div.getElementsByTag("a");
+            for(org.jsoup.nodes.Element link : links) {
+                if(!link.hasText()) continue;
+                printWriter.println(link.text() + "\t" + link.attr("href"));
+            }
+        }
+        printWriter.println();
+
+        ListDiv = doc.getElementsByAttributeValue("class", "mt30 mod_news_special");
+//        printWriter.println("新闻专题");
+        for(org.jsoup.nodes.Element div : ListDiv) {
+            Elements links = div.getElementsByTag("a");
+            for(org.jsoup.nodes.Element link : links) {
+                if(!link.hasText()) continue;
+                printWriter.println(link.text() + "\t" + link.attr("href"));
+            }
+        }
+        printWriter.println();
+
+        ListDiv = doc.getElementsByAttributeValue("class", "mt27 mod_high_dynamic");
+//        printWriter.println("高层动态");
+        for(org.jsoup.nodes.Element div : ListDiv) {
+            Elements links = div.getElementsByTag("a");
+            for(org.jsoup.nodes.Element link : links) {
+                if(!link.hasText()) continue;
+                printWriter.println(link.text() + "\t" + link.attr("href"));
+            }
+        }
+        printWriter.println();
+
+        ListDiv = doc.getElementsByAttributeValue("class", "mt35 mod_war");
+//        printWriter.println("军事航空");
+        for(org.jsoup.nodes.Element div : ListDiv) {
+            Elements links = div.getElementsByTag("a");
+            for(org.jsoup.nodes.Element link : links) {
+                if(!link.hasText()) continue;
+                printWriter.println(link.text() + "\t" + link.attr("href"));
+            }
+        }
+        printWriter.println();
+
+        ListDiv = doc.getElementsByAttributeValue("class", "mt35 mod_hot_rank clearfix");
+//        printWriter.println("热点排行");
+        for(org.jsoup.nodes.Element div : ListDiv) {
+            Elements links = div.getElementsByTag("a");
+            for(org.jsoup.nodes.Element link : links) {
+                if(!link.hasText()) continue;
+                printWriter.println(link.text() + "\t" + link.attr("href"));
+            }
+        }
+        printWriter.println();
+
+        ListDiv = doc.getElementsByAttributeValue("class", "mt35 mod_money");
+//        printWriter.println("财经");
+        for(org.jsoup.nodes.Element div : ListDiv) {
+            Elements links = div.getElementsByTag("a");
+            for(org.jsoup.nodes.Element link : links) {
+                if(!link.hasText()) continue;
+                printWriter.println(link.text() + "\t" + link.attr("href"));
+            }
+        }
+        printWriter.println();
+
+        ListDiv = doc.getElementsByAttributeValue("class", "mt27 mod_sports");
+//        printWriter.println("体育");
+        for(org.jsoup.nodes.Element div : ListDiv) {
+            Elements links = div.getElementsByTag("a");
+            for(org.jsoup.nodes.Element link : links) {
+                if(!link.hasText()) continue;
+                printWriter.println(link.text() + "\t" + link.attr("href"));
+            }
+        }
+        printWriter.println();
+
+        ListDiv = doc.getElementsByAttributeValue("class", "mt35 mod_ent");
+//        printWriter.println("娱乐");
+        for(org.jsoup.nodes.Element div : ListDiv) {
+            Elements links = div.getElementsByTag("a");
+            for(org.jsoup.nodes.Element link : links) {
+                if(!link.hasText()) continue;
+                printWriter.println(link.text() + "\t" + link.attr("href"));
+            }
+        }
+        printWriter.println();
+
+        ListDiv = doc.getElementsByAttributeValue("class", "mt25 mod_auto");
+//        printWriter.println("汽车精选");
+        for(org.jsoup.nodes.Element div : ListDiv) {
+            Elements links = div.getElementsByTag("a");
+            for(org.jsoup.nodes.Element link : links) {
+                if(!link.hasText()) continue;
+                printWriter.println(link.text() + "\t" + link.attr("href"));
+            }
+        }
+        printWriter.println();
+
+        ListDiv = doc.getElementsByAttributeValue("class", "mt35 mod_house");
+        printWriter.println("房产");
+        for(org.jsoup.nodes.Element div : ListDiv) {
+            Elements links = div.getElementsByTag("a");
+            for(org.jsoup.nodes.Element link : links) {
+                if(!link.hasText()) continue;
+                printWriter.println(link.text() + "\t" + link.attr("href"));
+            }
+        }
+        printWriter.println();
+
+        ListDiv = doc.getElementsByAttributeValue("class", "mt35 mod_wonderful_video");
+//        printWriter.println("精彩视频");
+        for(org.jsoup.nodes.Element div : ListDiv) {
+            Elements links = div.getElementsByTag("a");
+            for(org.jsoup.nodes.Element link : links) {
+                if(!link.hasText()) continue;
+                printWriter.println(link.text() + "\t" + link.attr("href"));
+            }
+        }
+        printWriter.println();
+
+        ListDiv = doc.getElementsByAttributeValue("class", "mt35 mod_tech");
+//        printWriter.println("科技");
+        for(org.jsoup.nodes.Element div : ListDiv) {
+            Elements links = div.getElementsByTag("a");
+            for(org.jsoup.nodes.Element link : links) {
+                if(!link.hasText()) continue;
+                printWriter.println(link.text() + "\t" + link.attr("href"));
+            }
+        }
+        printWriter.println();
+
+        ListDiv = doc.getElementsByAttributeValue("class", "mt27 mod_digi");
+//        printWriter.println("手机数码");
+        for(org.jsoup.nodes.Element div : ListDiv) {
+            Elements links = div.getElementsByTag("a");
+            for(org.jsoup.nodes.Element link : links) {
+                if(!link.hasText()) continue;
+                printWriter.println(link.text() + "\t" + link.attr("href"));
+            }
+        }
+        printWriter.println();
+
+        ListDiv = doc.getElementsByAttributeValue("class", "mt35 mod_lady");
+//        printWriter.println("女人时尚");
+        for(org.jsoup.nodes.Element div : ListDiv) {
+            Elements links = div.getElementsByTag("a");
+            for(org.jsoup.nodes.Element link : links) {
+                if(!link.hasText()) continue;
+                printWriter.println(link.text() + "\t" + link.attr("href"));
+            }
+        }
+        printWriter.println();
+
+        ListDiv = doc.getElementsByAttributeValue("class", "mt27 mod_edu");
+//        printWriter.println("教育旅游");
+        for(org.jsoup.nodes.Element div : ListDiv) {
+            Elements links = div.getElementsByTag("a");
+            for(org.jsoup.nodes.Element link : links) {
+                if(!link.hasText()) continue;
+                printWriter.println(link.text() + "\t" + link.attr("href"));
+            }
+        }
+        printWriter.println();
+
+        ListDiv = doc.getElementsByAttributeValue("class", "mt27 mod_jiankang");
+//        printWriter.println("健康读书");
+        for(org.jsoup.nodes.Element div : ListDiv) {
+            Elements links = div.getElementsByTag("a");
+            for(org.jsoup.nodes.Element link : links) {
+                if(!link.hasText()) continue;
+                printWriter.println(link.text() + "\t" + link.attr("href"));
+            }
+        }
+        printWriter.println();
+
+        ListDiv = doc.getElementsByAttributeValue("class", "ns_area index_media");
+        printWriter.println("媒体合作");
+        for(org.jsoup.nodes.Element div : ListDiv) {
+            Elements links = div.getElementsByTag("a");
+            for(org.jsoup.nodes.Element link : links) {
+                if(!link.hasText()) continue;
+                printWriter.println(link.text() + "\t" + link.attr("href"));
+            }
+        }
+        printWriter.println();
 
     }
 
